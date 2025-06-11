62a1505cd4ee431283b428c8f10654748c58d461
Cauê Jannini <cauejannini@gmail.com>
2025-06-11 16:08:55 -0300

    fixed csv formatting and added anexos

--- composeApp/src/wasmJsMain/kotlin/org/cidadeape/monitoraorcamento/common/ComposeUtil.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/org/cidadeape/monitoraorcamento/common/ComposeUtil.wasmJs.kt
@@ -21,85 +21,96 @@ actual fun OpenInNewWindow(url: String) {
 actual fun DownloadCsv(fileName: String, listaEmpenhos: List<Empenho>) {
     val csvDataBuilder = StringBuilder()
 
+    val csvSeparator = ";"
     // CSV Headers
-    csvDataBuilder.append("codEmpresa")
-    csvDataBuilder.append(",")
-    csvDataBuilder.append("nomEmpresa")
-    csvDataBuilder.append(",")
-    csvDataBuilder.append("codEmpenho")
-    csvDataBuilder.append(",")
-    csvDataBuilder.append("anoEmpenho")
-    csvDataBuilder.append(",")
-    csvDataBuilder.append("mesEmpenho")
-    csvDataBuilder.append(",")
-    csvDataBuilder.append("datEmpenho")
-    csvDataBuilder.append(",")
-    csvDataBuilder.append("codProcesso")
-    csvDataBuilder.append(",")
-    csvDataBuilder.append("numCpfCnpj")
-    csvDataBuilder.append(",")
-    csvDataBuilder.append("txtRazaoSocial")
-    csvDataBuilder.append(",")
-    csvDataBuilder.append("numContrato")
-    csvDataBuilder.append(",")
-    csvDataBuilder.append("anoContrato")
-    csvDataBuilder.append(",")
     csvDataBuilder.append("codProjetoAtividade")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("txDescricaoProjetoAtividade")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
+    csvDataBuilder.append("codEmpresa")
+    csvDataBuilder.append(csvSeparator)
+    csvDataBuilder.append("nomEmpresa")
+    csvDataBuilder.append(csvSeparator)
+    csvDataBuilder.append("codEmpenho")
+    csvDataBuilder.append(csvSeparator)
+    csvDataBuilder.append("anoEmpenho")
+    csvDataBuilder.append(csvSeparator)
+    csvDataBuilder.append("mesEmpenho")
+    csvDataBuilder.append(csvSeparator)
+    csvDataBuilder.append("datEmpenho")
+    csvDataBuilder.append(csvSeparator)
+    csvDataBuilder.append("codProcesso")
+    csvDataBuilder.append(csvSeparator)
+    csvDataBuilder.append("numCpfCnpj")
+    csvDataBuilder.append(csvSeparator)
+    csvDataBuilder.append("txtRazaoSocial")
+    csvDataBuilder.append(csvSeparator)
+    csvDataBuilder.append("numContrato")
+    csvDataBuilder.append(csvSeparator)
+    csvDataBuilder.append("anoContrato")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("codCategoria")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("txDescricaoCategoriaEconomica")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("codGrupo")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("txDescricaoGrupoDespesa")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("codFonteRecurso")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("txDescricaoFonteRecurso")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("codOrgao")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("txDescricaoOrgao")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("codUnidade")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("txDescricaoUnidade")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("codElemento")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("txDescricaoElemento")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("codFuncao")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("txDescricaoFuncao")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("codSubFuncao")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("txDescricaoSubFuncao")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("codItemDespesa")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("txDescricaoItemDespesa")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
+    csvDataBuilder.append("anexos")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("valTotalEmpenhado")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("valAnuladoEmpenho")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("valEmpenhadoLiquido")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("valLiquidado")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("valPagoExercicio")
-    csvDataBuilder.append(",")
+    csvDataBuilder.append(csvSeparator)
     csvDataBuilder.append("valPagoRestos")
     csvDataBuilder.append("\n")
 
     for (i in listaEmpenhos.indices) {
         val empenho = listaEmpenhos[i]
+
+        val anexoDesc = empenho.anexos?.joinToString(" | ") {
+            "${it.descricaoAnexo} - ${it.qtdeAnexo} ${it.siglaUnidadeMedida} - " +
+                    "Valor unitário: ${Util.formatToCurrency(it.valorUnitarioAnexo)}"
+        }?.replace("\n","") ?: " "
+
         val values = arrayOf(
+            empenho.codProjetoAtividade,
+            empenho.txDescricaoProjetoAtividade,
             empenho.codEmpresa,
             empenho.nomEmpresa,
             empenho.codEmpenho,
@@ -111,8 +122,6 @@ actual fun DownloadCsv(fileName: String, listaEmpenhos: List<Empenho>) {
             empenho.txtRazaoSocial,
             empenho.numContrato,
             empenho.anoContrato,
-            empenho.codProjetoAtividade,
-            empenho.txDescricaoProjetoAtividade,
             empenho.codCategoria,
             empenho.txDescricaoCategoriaEconomica,
             empenho.codGrupo,
@@ -131,6 +140,7 @@ actual fun DownloadCsv(fileName: String, listaEmpenhos: List<Empenho>) {
             empenho.txDescricaoSubFuncao,
             empenho.codItemDespesa,
             empenho.txDescricaoItemDespesa,
+            anexoDesc,
             empenho.valTotalEmpenhado.formatToBrasil(),
             empenho.valAnuladoEmpenho.formatToBrasil(),
             empenho.valEmpenhadoLiquido.formatToBrasil(),
@@ -138,7 +148,7 @@ actual fun DownloadCsv(fileName: String, listaEmpenhos: List<Empenho>) {
             empenho.valPagoExercicio.formatToBrasil(),
             empenho.valPagoRestos.formatToBrasil()
         )
-        csvDataBuilder.append(values.joinToString(",")).append("\n")
+        csvDataBuilder.append(values.joinToString(csvSeparator)).append("\n")
     }
     val jsArray = JsArray<JsAny?>()
     jsArray[0] = csvDataBuilder.toString().toJsString()
